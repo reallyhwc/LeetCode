e8a50e95d78719c5d317746ffddbc132c9fdca6e
xuhu <reallyhwc@foxmail.com>
2019-10-14 19:14:47 +0800

    easy 00027 删除排序数组中的重复项

---
+++ src/com/xuhu/LeetCode/easy0026/Main.java
@@ -0,0 +1,9 @@
+package com.xuhu.LeetCode.easy0026;
+
+public class Main {
+    public static void main(String[] args){
+        int[] nums = {1,1,1,1,1,2,2,2,2,2,3,3,3,3,3,4,4,4,4,4};
+        Solution solution = new Solution();
+        System.out.println(solution.removeDuplicates(nums));
+    }
+}
