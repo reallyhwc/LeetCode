d72b7cb0383e41299975b0f3045651e3f8f51390
xuhu <reallyhwc@foxmail.com>
2019-10-15 23:03:11 +0800

    easy 00038 报数

---
+++ src/com/xuhu/LeetCode/easy0038/Main.java
@@ -0,0 +1,8 @@
+package com.xuhu.LeetCode.easy0038;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.countAndSay(5));
+    }
+}
