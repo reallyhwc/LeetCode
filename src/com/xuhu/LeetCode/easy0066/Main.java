fb45df126b992d8df405192512cb1a19dd95ea08
xuhu <reallyhwc@foxmail.com>
2019-10-21 21:39:13 +0800

    easy 0066 加一

---
+++ src/com/xuhu/LeetCode/easy0066/Main.java
@@ -0,0 +1,14 @@
+package com.xuhu.LeetCode.easy0066;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        int[] nums = {4,3,2,1};
+        int[] nums2 = {9,9,9,9,9,8,9};
+
+
+        for(int X : solution.plusOne(nums2)){
+            System.out.println(X);
+        }
+    }
+}
