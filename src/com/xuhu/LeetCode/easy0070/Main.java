aa18b272b2c1f69fa1de780b9ddbba026ef15be9
xuhu <reallyhwc@foxmail.com>
2019-10-22 22:38:28 +0800

    easy 0070 爬楼梯

---
+++ src/com/xuhu/LeetCode/easy0070/Main.java
@@ -0,0 +1,8 @@
+package com.xuhu.LeetCode.easy0070;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.climbStairs(3));
+    }
+}
