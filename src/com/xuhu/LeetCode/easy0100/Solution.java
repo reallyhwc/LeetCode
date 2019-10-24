a881e2e7e71b9710b197af2174a049b4591f737f
xuhu <reallyhwc@foxmail.com>
2019-10-24 21:10:52 +0800

    0100相同的树

---
+++ src/com/xuhu/LeetCode/easy0100/Solution.java
@@ -0,0 +1,17 @@
+package com.xuhu.LeetCode.easy0100;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        return false;
+    }
+}
---
+++ src/com/xuhu/LeetCode/easy0100/TreeNode.java
@@ -0,0 +1,12 @@
+package com.xuhu.LeetCode.easy0100;
+
+/**
+ * @author xuhu
+ * @date 2019-10-24 21:10
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
