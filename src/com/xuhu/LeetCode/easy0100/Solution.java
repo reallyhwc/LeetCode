84895991739f657ad39793e29db61dd2aedacb7b
xuhu <reallyhwc@foxmail.com>
2019-10-26 10:38:57 +0800

    0100 相同的树

--- src/com/xuhu/LeetCode/easy0100/Solution.java
+++ src/com/xuhu/LeetCode/easy0100/Solution.java
@@ -11,7 +11,15 @@ package com.xuhu.LeetCode.easy0100;
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-
-        return false;
+        //运用递归的思想
+        //两个都为空 true
+        if(p == null && q == null){
+            return true;
+        }
+        //只有一个为空 false
+        if(p == null || q == null){
+            return false;
+        }
+        return p.val == q.val ? (isSameTree(p.left,q.left) && isSameTree(p.right,q.right)) : false;
     }
 }
