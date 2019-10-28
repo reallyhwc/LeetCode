a8bc44b5e3ad82e5054ddf695a4c735f9f448dc7
xuhu <reallyhwc@foxmail.com>
2019-10-28 20:33:34 +0800

    0110 平衡二叉树

---
+++ src/com/xuhu/LeetCode/easy0110/Main.java
@@ -0,0 +1,13 @@
+package com.xuhu.LeetCode.easy0110;
+
+import com.xuhu.LeetCode.a.util.CreateBinaryTreeByArray;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        Integer[] array1 = {3,9,20,null,null,15,7};
+        Integer[] array2 = {1,2,2,3,3,null,null,4,4};
+        System.out.println(solution.isBalanced(CreateBinaryTreeByArray.createBinaryTreeByArray(array1,0)));
+        System.out.println(solution.isBalanced(CreateBinaryTreeByArray.createBinaryTreeByArray(array2,0)));
+    }
+}
