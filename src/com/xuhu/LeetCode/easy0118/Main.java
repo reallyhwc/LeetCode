4fea73625bee17714566c01673bb3e4c1a469f5a
xuhu <reallyhwc@foxmail.com>
2019-10-29 22:05:13 +0800

    0118杨辉三角

---
+++ src/com/xuhu/LeetCode/easy0118/Main.java
@@ -0,0 +1,17 @@
+package com.xuhu.LeetCode.easy0118;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        List<List<Integer>> lists = solution.generate(30);
+        for(List<Integer> list : lists){
+            for(Integer X : list){
+                System.out.print(X + "\t\t");
+            }
+            System.out.println();
+        }
+
+    }
+}
