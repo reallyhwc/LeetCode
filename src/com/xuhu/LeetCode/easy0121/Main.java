69f1ef0a3e15b5420fc4e3fd6c1ff1a591262942
xuhu <reallyhwc@foxmail.com>
2019-10-30 17:16:50 +0800

    0121 买股票的最佳时机

---
+++ src/com/xuhu/LeetCode/easy0121/Main.java
@@ -0,0 +1,11 @@
+package com.xuhu.LeetCode.easy0121;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        int[] array = {7,1,5,3,6,4};
+        int[] array1 = {7,6,4,3,1};
+        System.out.println(solution.maxProfit(array));
+        System.out.println(solution.maxProfit(array1));
+    }
+}
---
+++ src/com/xuhu/LeetCode/easy0121/Solution.java
@@ -0,0 +1,17 @@
+package com.xuhu.LeetCode.easy0121;
+
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(null == prices || prices.length <= 1){
+            return 0;
+        }
+        int min = prices[0];
+        int max = 0;
+        for(int i = 1; i < prices.length; i++){
+            max = Math.max(max,prices[i] - min);
+            min = Math.min(min,prices[i]);
+        }
+        return max;
+    }
+}
