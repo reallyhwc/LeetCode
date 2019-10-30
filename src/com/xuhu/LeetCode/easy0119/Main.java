71eeac423afe928b09455b70ece628b51dd368d1
xuhu <reallyhwc@foxmail.com>
2019-10-30 15:48:51 +0800

    0119 杨辉三角II

---
+++ src/com/xuhu/LeetCode/easy0119/Main.java
@@ -0,0 +1,11 @@
+package com.xuhu.LeetCode.easy0119;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        for(Integer X : solution.getRow(5)){
+            System.out.print(X + "\t\t");
+        }
+
+    }
+}
