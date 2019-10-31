8f98d626faed17eb3bff4a8cbf6a4d7caa959931
xuhu <reallyhwc@foxmail.com>
2019-10-31 19:26:16 +0800

    0141 环形链表  以及部分图片，ListNode公共类

---
+++ src/com/xuhu/LeetCode/a/util/ListNode.java
@@ -0,0 +1,14 @@
+package com.xuhu.LeetCode.a.util;
+
+/**
+ * @author xuhu
+ * @date 2019-10-31 9:55
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x){
+        val = x;
+        next = null;
+    }
+}
