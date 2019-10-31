f1cd6059d71b20aa3863c55b86a947938901823c
xuhu <reallyhwc@foxmail.com>
2019-10-31 09:48:27 +0800

    0136 只出现一次的数组

---
+++ src/com/xuhu/LeetCode/easy0136/Main.java
@@ -0,0 +1,9 @@
+package com.xuhu.LeetCode.easy0136;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        int[] array = {4,1,2,1,2};
+        System.out.println(solution.singleNumber(array));
+    }
+}
