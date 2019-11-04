dc9525bf231933eaad0f77d6eb606b301fce0876
xuhu <reallyhwc@foxmail.com>
2019-11-04 20:04:07 +0800

    0167 两数之和 II - 输入有序数组--双指针两头逼近

--- src/com/xuhu/LeetCode/easy0167/Main.java
+++ src/com/xuhu/LeetCode/easy0167/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args){
         Solution solution = new Solution();
         int[] arrays = {2, 7, 11, 15};
-        int[] array = solution.twoSum(arrays,9);
+        int[] array = solution.twoSum2(arrays,9);
         System.out.println(array[0]);
         System.out.println(array[1]);
     }
