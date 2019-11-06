2ca80e578d7fde8ec693525af204bee90c6754e4
xuhu <reallyhwc@foxmail.com>
2019-11-06 18:59:03 +0800

    ## 0172 阶乘后的0

---
+++ src/com/xuhu/LeetCode/easy0172/Main.java
@@ -0,0 +1,10 @@
+package com.xuhu.LeetCode.easy0172;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.trailingZeroes(5));
+
+        System.out.println(solution.trailingZeroes(10));
+    }
+}
