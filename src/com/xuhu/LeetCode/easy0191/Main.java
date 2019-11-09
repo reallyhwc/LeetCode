896f9449ff2f34e6d0274f3c6fbbff860c5b9332
xuhu <reallyhwc@foxmail.com>
2019-11-09 10:48:47 +0800

    0191 位1的个数

---
+++ src/com/xuhu/LeetCode/easy0191/Main.java
@@ -0,0 +1,8 @@
+package com.xuhu.LeetCode.easy0191;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.hammingWeight(-3));
+    }
+}
