9ddf1ada9256ec25c80c4dcbaefb775750459ca8
xuhu <reallyhwc@foxmail.com>
2019-11-14 19:41:19 +0800

    0205 同构字符串

--- src/com/xuhu/LeetCode/easy/easyBefore300/easy0205/Main.java
+++ src/com/xuhu/LeetCode/easy/easyBefore300/easy0205/Main.java
@@ -3,6 +3,14 @@ package com.xuhu.LeetCode.easy.easyBefore300.easy0205;
 public class Main {
     public static void main(String[] args){
         Solution solution = new Solution();
-        System.out.println();
+        String s = "egg";
+        String t = "add";
+        String s1 = "foo";
+        String t1 = "bar";
+        String s2 = "paper";
+        String t2 = "title";
+        System.out.println(solution.isIsomorphic(s, t));
+        System.out.println(solution.isIsomorphic(s1, t1));
+        System.out.println(solution.isIsomorphic(s2, t2));
     }
 }
--- src/com/xuhu/LeetCode/easy/easyBefore300/easy0205/Solution.java
+++ src/com/xuhu/LeetCode/easy/easyBefore300/easy0205/Solution.java
@@ -2,4 +2,13 @@ package com.xuhu.LeetCode.easy.easyBefore300.easy0205;
 
 
 class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int length = s.length();
+        for(int i = 0 ;i < length; i++){
+            if(s.indexOf(s.charAt(i)) != (t.indexOf(t.charAt(i)))){
+                return false;
+            }
+        }
+        return true;
+    }
 }
