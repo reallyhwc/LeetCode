580f10c2af318b84348a2247677145f9342e935d
xuhu <reallyhwc@foxmail.com>
2019-11-26 12:35:50 +0800

    0232 用栈实现队列

--- src/com/xuhu/LeetCode/easy/easyBefore300/easy0232/Main.java
+++ src/com/xuhu/LeetCode/easy/easyBefore300/easy0232/Main.java
@@ -2,7 +2,12 @@ package com.xuhu.LeetCode.easy.easyBefore300.easy0232;
 
 public class Main {
     public static void main(String[] args){
-        Solution solution = new Solution();
-        System.out.println();
+        MyQueue queue = new MyQueue();
+        queue.push(1);
+        queue.push(2);
+
+        System.out.println(queue.peek());
+        System.out.println(queue.pop());
+        System.out.println(queue.empty());
     }
 }
--- src/com/xuhu/LeetCode/easy/easyBefore300/easy0232/MyQueue.java
+++ src/com/xuhu/LeetCode/easy/easyBefore300/easy0232/MyQueue.java
@@ -1,34 +1,50 @@
 package com.xuhu.LeetCode.easy.easyBefore300.easy0232;
 
+import java.util.Stack;
+
 /**
  * @author xuhu
  * @date 2019-11-21 19:57
  */
 class MyQueue {
 
+    private Stack<Integer> a;
+    private Stack<Integer> b;
+
     /** Initialize your data structure here. */
     public MyQueue() {
-
+        a = new Stack<>();
+        b = new Stack<>();
     }
 
     /** Push element x to the back of queue. */
     public void push(int x) {
-
+        a.push(x);
     }
 
     /** Removes the element from in front of queue and returns that element. */
     public int pop() {
-
+        if(b.isEmpty()){
+            while (!a.isEmpty()){
+                b.push(a.pop());
+            }
+        }
+        return b.pop();
     }
 
     /** Get the front element. */
     public int peek() {
-
+        if(b.isEmpty()){
+            while (!a.isEmpty()){
+                b.push(a.pop());
+            }
+        }
+        return b.peek();
     }
 
     /** Returns whether the queue is empty. */
     public boolean empty() {
-
+        return a.isEmpty() && b.isEmpty();
     }
 }
 
