779d57c7b5bf5b14eba502e4a812ce692f0aec2b
xuhu <reallyhwc@foxmail.com>
2019-12-25 19:48:42 +0800

    0405 数字转换为十六进制数

--- src/com/xuhu/LeetCode/easy/easyBefore500/easy0405/Solution.java
+++ src/com/xuhu/LeetCode/easy/easyBefore500/easy0405/Solution.java
@@ -1,6 +1,8 @@
 package com.xuhu.LeetCode.easy.easyBefore500.easy0405;
 
 class Solution {
+
+    //本身计算机存储的就是数字的补码，所以直接每四位二进制数转换成一个16进制数即可
     public String toHex(int num) {
         if (num == 0) return "0";
         char[] chars = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
