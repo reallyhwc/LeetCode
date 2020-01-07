5542e9abaca8a49e20c07f7ed941ca5ac10a3648
xuhu <reallyhwc@foxmail.com>
2020-01-07 20:28:44 +0800

    0415 字符串相加

--- src/com/xuhu/LeetCode/easy/easyBefore500/easy0415/Solution.java
+++ src/com/xuhu/LeetCode/easy/easyBefore500/easy0415/Solution.java
@@ -3,9 +3,36 @@ package com.xuhu.LeetCode.easy.easyBefore500.easy0415;
 
 class Solution {
 
+    public String addStrings(String num1, String num2) {
+        if(num1.length() > num2.length()){
+            return addStrings(num2,num1);
+        }
+        StringBuffer sb = new StringBuffer();
+        int add = 0;
+        for(int i = 0; i < num2.length(); i++){
+            int a = (num1.length() > i) ? num1.charAt(num1.length() - 1 - i) - 48 : 0;
+            int b = num2.charAt(num2.length() - 1 - i) - 48;
+            sb.append((a + b + add) % 10);
+            add = (a + b + add) / 10;
+        }
+        if(add == 1){
+            sb.append(1);
+        }
+        return sb.reverse().toString();
+    }
+
+
+
+
+
+
+
+
+
+
     //时间复杂度太高，再来一次
     private int add = 0;
-    public String addStrings(String num1, String num2) {
+    public String addStrings1(String num1, String num2) {
         num1 = (new StringBuffer(num1).reverse().toString());
         num2 = (new StringBuffer(num2).reverse().toString());
         StringBuffer result = new StringBuffer();
