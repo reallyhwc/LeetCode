b5b3eabe64b28550dd75f67dd87ee54c69c1d1bf
xuhu <reallyhwc@foxmail.com>
2020-01-08 20:00:30 +0800

    0434 字符串中的单词数

--- src/com/xuhu/LeetCode/easy/easyBefore500/easy0434/Solution.java
+++ src/com/xuhu/LeetCode/easy/easyBefore500/easy0434/Solution.java
@@ -2,11 +2,21 @@ package com.xuhu.LeetCode.easy.easyBefore500.easy0434;
 
 
 class Solution {
-    public int countSegments(String s) {
+    public int countSegments1(String s) {
         String trimmed = s.trim();
         if (trimmed.equals("")) {
             return 0;
         }
         return trimmed.split("\\s+").length;
+    }
+
+    public int countSegments(String s) {
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if((i==0||s.charAt(i-1)==' ')&&s.charAt(i)!=' '){
+                count++;
+            }
+        }
+        return count;
     }
 }
