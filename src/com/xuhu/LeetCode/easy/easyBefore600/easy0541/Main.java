72fecbfcb760400d8a93fdcbc6da40a2a3da1fea
xuhu <reallyhwc@foxmail.com>
2020-03-10 08:47:28 +0800

    0541 反转字符串II

---
+++ src/com/xuhu/LeetCode/easy/easyBefore600/easy0541/Main.java
@@ -0,0 +1,8 @@
+package com.xuhu.LeetCode.easy.easyBefore600.easy0541;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.reverseStr("abcdefg",2));
+    }
+}
