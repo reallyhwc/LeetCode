54da0dcd88ca95ef4a44c5236ac94059b23514c0
xuhu <reallyhwc@foxmail.com>
2020-04-05 23:19:00 +0800

    0849 到最近人的最大距离

---
+++ src/com/xuhu/LeetCode/easy/easyBefore900/easy849/Main.java
@@ -0,0 +1,11 @@
+package com.xuhu.LeetCode.easy.easyBefore900.easy849;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        int[] array = {1,0,0,0,1,0,1};
+        int[] array1 = {1,0,0,0};
+        System.out.println(solution.maxDistToClosest(array));
+        System.out.println(solution.maxDistToClosest(array1));
+    }
+}
