26e4453611f7cece26df3de69b9b7070a0220dae
xuhu <reallyhwc@foxmail.com>
2020-04-05 23:23:50 +0800

    0852 山脉数组的巅峰索引

---
+++ src/com/xuhu/LeetCode/easy/easyBefore900/easy852/Main.java
@@ -0,0 +1,11 @@
+package com.xuhu.LeetCode.easy.easyBefore900.easy852;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        int[] array = {0,1,0};
+        int[] array1 = {0,2,1,0};
+        System.out.println(solution.peakIndexInMountainArray(array));
+        System.out.println(solution.peakIndexInMountainArray(array1));
+    }
+}
---
+++ src/com/xuhu/LeetCode/easy/easyBefore900/easy852/Solution.java
@@ -0,0 +1,12 @@
+package com.xuhu.LeetCode.easy.easyBefore900.easy852;
+
+class Solution {
+    public int peakIndexInMountainArray(int[] A) {
+        for(int i = 0; i < A.length-1; i++){
+            if(A[i] > A[i+1]){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
