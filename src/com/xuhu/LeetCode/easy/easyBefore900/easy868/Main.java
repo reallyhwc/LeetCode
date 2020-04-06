daaa3362d46f3120f0c4eb25f5aab568e215e5d1
xuhu <reallyhwc@foxmail.com>
2020-04-06 14:11:56 +0800

    0868 二进制间距

---
+++ src/com/xuhu/LeetCode/easy/easyBefore900/easy868/Main.java
@@ -0,0 +1,11 @@
+package com.xuhu.LeetCode.easy.easyBefore900.easy868;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.binaryGap(22));
+        System.out.println(solution.binaryGap(5));
+        System.out.println(solution.binaryGap(6));
+        System.out.println(solution.binaryGap(8));
+    }
+}
