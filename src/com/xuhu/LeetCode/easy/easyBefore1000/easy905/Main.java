5609c56adc4900ff6ee9ed3ac58d9dc3c3503c92
xuhu <reallyhwc@foxmail.com>
2020-04-07 21:56:30 +0800

    0905 按奇偶排序数组

---
+++ src/com/xuhu/LeetCode/easy/easyBefore1000/easy905/Main.java
@@ -0,0 +1,10 @@
+package com.xuhu.LeetCode.easy.easyBefore1000.easy905;
+
+import com.xuhu.LeetCode.a.util.ListUtils;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        ListUtils.pringlnArrays(solution.sortArrayByParity(new int[] {3,1,2,4}));
+    }
+}
