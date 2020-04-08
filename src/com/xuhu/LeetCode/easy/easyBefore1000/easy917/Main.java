02f669370aaae4ec946f863121d95f4215f51112
xuhu <reallyhwc@foxmail.com>
2020-04-08 10:56:57 +0800

    0917 仅仅反转字母

---
+++ src/com/xuhu/LeetCode/easy/easyBefore1000/easy917/Main.java
@@ -0,0 +1,10 @@
+package com.xuhu.LeetCode.easy.easyBefore1000.easy917;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.reverseOnlyLetters("ab-cd"));
+        System.out.println(solution.reverseOnlyLetters("a-bC-dEf-ghIj"));
+        System.out.println(solution.reverseOnlyLetters("Test1ng-Leet=code-Q!"));
+    }
+}
