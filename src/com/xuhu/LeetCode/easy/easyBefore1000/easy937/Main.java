f2f3cc1fd89e2cdb29646723ba0e290c68d187ad
xuhu <reallyhwc@foxmail.com>
2020-04-08 19:14:22 +0800

    0937 重新排列日志文件

---
+++ src/com/xuhu/LeetCode/easy/easyBefore1000/easy937/Main.java
@@ -0,0 +1,10 @@
+package com.xuhu.LeetCode.easy.easyBefore1000.easy937;
+
+import com.xuhu.LeetCode.a.util.ListUtils;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        ListUtils.printlnStrs(solution.reorderLogFiles(new String[] {"a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"}));
+    }
+}
