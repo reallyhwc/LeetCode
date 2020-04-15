183bc2039af50c6997b4fd4d8185617e3ef7ac39
xuhu <reallyhwc@foxmail.com>
2020-04-15 14:44:30 +0800

    0029 两数相除

---
+++ src/com/xuhu/LeetCode/mid/midBefore100/mid0029/Main.java
@@ -0,0 +1,8 @@
+package com.xuhu.LeetCode.mid.midBefore100.mid0029;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.divide(100,3));
+    }
+}
