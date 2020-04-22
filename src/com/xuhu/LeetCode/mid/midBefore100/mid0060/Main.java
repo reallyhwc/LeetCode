1e0d2e291a2da5db4cc2c37a1cc09d67de04c587
xuhu <reallyhwc@foxmail.com>
2020-04-22 15:29:54 +0800

    0060 第k个排列

---
+++ src/com/xuhu/LeetCode/mid/midBefore100/mid0060/Main.java
@@ -0,0 +1,12 @@
+package com.xuhu.LeetCode.mid.midBefore100.mid0060;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.getPermutation(5,1));
+        System.out.println(solution.getPermutation(5,2));
+        System.out.println(solution.getPermutation(5,3));
+        System.out.println(solution.getPermutation(5,4));
+        System.out.println(solution.getPermutation(5,5));
+    }
+}
