090e1ea0029993139c287b26702253c58fe1215b
xuhu <reallyhwc@foxmail.com>
2020-04-23 15:45:23 +0800

    0062 不同路径

---
+++ src/com/xuhu/LeetCode/mid/midBefore100/mid0062/Main.java
@@ -0,0 +1,8 @@
+package com.xuhu.LeetCode.mid.midBefore100.mid0062;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.uniquePaths(20,20));
+    }
+}
