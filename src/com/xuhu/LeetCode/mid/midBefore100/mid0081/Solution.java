8e9bfa6bd8e62e4bb679f7b3efd147cae4fd251e
xuhu <reallyhwc@foxmail.com>
2020-04-26 22:23:45 +0800

    0081 搜索旋转排序树组II-- 补充大神方法

--- src/com/xuhu/LeetCode/mid/midBefore100/mid0081/Solution.java
+++ src/com/xuhu/LeetCode/mid/midBefore100/mid0081/Solution.java
@@ -10,4 +10,33 @@ class Solution {
         }
         return false;
     }
+
+    public boolean search1(int[] nums, int target) {
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(nums[mid]==target)
+                return true;
+            if(nums[start]<nums[mid]){
+                if(nums[mid]>=target&&target>=nums[start]){
+                    end=mid-1;
+                }else{
+                    start=mid+1;
+                }
+            }else if(nums[end]>nums[mid]){
+                if(nums[mid]<=target&&target<=nums[end]){
+                    start=mid+1;
+                }else{
+                    end=mid-1;
+                }
+            }else{
+                if(nums[mid]==nums[start])
+                    start++;
+                else
+                    end--;
+            }
+        }
+        return false;
+    }
 }
