53d3f9d25da3d2632f1353cdfb90f79978f0cf13
xuhu <reallyhwc@foxmail.com>
2020-04-26 15:27:33 +0800

    0075 搜索二维矩阵

---
+++ src/com/xuhu/LeetCode/mid/midBefore100/mid0075/Main.java
@@ -0,0 +1,9 @@
+package com.xuhu.LeetCode.mid.midBefore100.mid0075;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        int[][] array = new int[][] {{1}};
+        System.out.println(solution.searchMatrix(array,0));
+    }
+}
---
+++ src/com/xuhu/LeetCode/mid/midBefore100/mid0075/Solution.java
@@ -0,0 +1,22 @@
+package com.xuhu.LeetCode.mid.midBefore100.mid0075;
+
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length == 0){
+            return false;
+        }
+        int row = 0;
+        int col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0){
+            if(matrix[row][col] < target){
+                row++;
+            }else if(matrix[row][col] > target){
+                col--;
+            }else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
