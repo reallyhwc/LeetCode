c888fb1f1f05331ac031faf803625ca94604cc05
xuhu <reallyhwc@foxmail.com>
2020-04-27 10:35:11 +0800

    0089 格雷编码

---
+++ src/com/xuhu/LeetCode/mid/midBefore100/mid0089/Main.java
@@ -0,0 +1,10 @@
+package com.xuhu.LeetCode.mid.midBefore100.mid0089;
+
+import com.xuhu.LeetCode.a.util.ListUtils;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        ListUtils.pringlnList(solution.grayCode(2));
+    }
+}
---
+++ src/com/xuhu/LeetCode/mid/midBefore100/mid0089/Solution.java
@@ -0,0 +1,28 @@
+package com.xuhu.LeetCode.mid.midBefore100.mid0089;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    /**
+     关键是搞清楚格雷编码的生成过程, G(i) = i ^ (i/2);
+     如 n = 3:
+     G(0) = 000,
+     G(1) = 1 ^ 0 = 001 ^ 000 = 001
+     G(2) = 2 ^ 1 = 010 ^ 001 = 011
+     G(3) = 3 ^ 1 = 011 ^ 001 = 010
+     G(4) = 4 ^ 2 = 100 ^ 010 = 110
+     G(5) = 5 ^ 2 = 101 ^ 010 = 111
+     G(6) = 6 ^ 3 = 110 ^ 011 = 101
+     G(7) = 7 ^ 3 = 111 ^ 011 = 100
+     **/
+    public List<Integer> grayCode(int n) {
+        List<Integer> ans = new ArrayList<>();
+
+        for(int i = 0; i < 1 << n; i++){
+            ans.add(i ^ i >> 1);
+        }
+        return ans;
+    }
+}
