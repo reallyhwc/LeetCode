d603a07bbd767c64b5086cecddc1b4d90fd4e671
xuhu <reallyhwc@foxmail.com>
2020-05-25 20:03:25 +0800

    0137 只出现一次的数字II

---
+++ src/com/xuhu/LeetCode/mid/midBefore200/mid0137/Main.java
@@ -0,0 +1,9 @@
+package com.xuhu.LeetCode.mid.midBefore200.mid0137;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.singleNumber(new int[]{2,2,3,2}));
+        System.out.println(solution.singleNumber(new int[]{0,1,0,1,0,1,99}));
+    }
+}
