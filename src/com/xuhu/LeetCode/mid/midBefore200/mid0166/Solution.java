5ba4ee31b8c201e408f92888e84ae99185210b12
xuhu <reallyhwc@foxmail.com>
2020-06-23 09:40:51 +0800

    0166 分数到小数 调整注释

--- src/com/xuhu/LeetCode/mid/midBefore200/mid0166/Solution.java
+++ src/com/xuhu/LeetCode/mid/midBefore200/mid0166/Solution.java
@@ -9,18 +9,25 @@ class Solution {
     public String fractionToDecimal(int numerator, int denominator) {
         if(numerator == 0) return "0";
         StringBuilder sb = new StringBuilder();
+        // 两个相同则为 false 两个不相同则为true （说明符号不同，拼上一个负号）
         if(numerator < 0 ^ denominator < 0){
             sb.append("-");
         }
-
+        // 被除数
         long dividend = Math.abs(Long.valueOf(numerator));
+        // 除数
         long divisor = Math.abs(Long.valueOf(denominator));
+        // 拼接小数点前的值
         sb.append(String.valueOf(dividend/divisor));
         long remainder = dividend % divisor;
+        // 如果能整除，那么直接返回前半段结果即可
         if(remainder == 0){
             return sb.toString();
         }
+        // 说明不能整除 拼接上小数点
         sb.append(".");
+        // map中存放 当前余数+当前字符串长度信息
+        // 如果当前余数相等了，那么说明存在了循环 在相应的字符串地方补上括号即可
         Map<Long,Integer> map = new HashMap<>();
         while (remainder != 0){
             if (map.containsKey(remainder)){
