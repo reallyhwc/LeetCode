cc31e258cd902a386122710808fee8c6aef3e79e
xuhu <reallyhwc@foxmail.com>
2020-07-08 11:20:19 +0800

    0222 完全二叉树的节点个数、

---
+++ src/com/xuhu/LeetCode/mid/midBefore300/mid0222/Main.java
@@ -0,0 +1,10 @@
+package com.xuhu.LeetCode.mid.midBefore300.mid0222;
+
+import com.xuhu.LeetCode.a.util.TreeUtils;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.countNodes(TreeUtils.create(new Integer[]{1,2,3,4,5,6},0)));
+    }
+}
