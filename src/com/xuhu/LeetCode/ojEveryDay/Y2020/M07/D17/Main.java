788dcc7ac0ada61fa757ad34d87370228816ea04
xuhu <reallyhwc@foxmail.com>
2020-07-17 22:55:12 +0800

    2020-07-17 0035 搜索插入位置

---
+++ src/com/xuhu/LeetCode/ojEveryDay/Y2020/M07/D17/Main.java
@@ -0,0 +1,8 @@
+package com.xuhu.LeetCode.ojEveryDay.Y2020.M07.D17;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.searchInsert(new int[]{1,3,5,6},5));
+    }
+}
