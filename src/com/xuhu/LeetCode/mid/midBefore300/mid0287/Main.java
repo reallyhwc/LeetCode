1ea8c0ee5f356df20f70ce12a4c00deae1c84fa8
xuhu <reallyhwc@foxmail.com>
2020-07-20 10:51:48 +0800

    0287 寻找重复数

--- src/com/xuhu/LeetCode/mid/midBefore300/mid0287/Main.java
+++ src/com/xuhu/LeetCode/mid/midBefore300/mid0287/Main.java
@@ -3,6 +3,9 @@ package com.xuhu.LeetCode.mid.midBefore300.mid0287;
 public class Main {
     public static void main(String[] args){
         Solution solution = new Solution();
-        System.out.println();
+        System.out.println(solution.findDuplicate(new int[]{1,3,4,2,2}));
+        System.out.println(solution.findDuplicate(new int[]{3,1,3,4,2}));
+        System.out.println(solution.findDuplicate(new int[]{1,2,3,4,5,5}));
+        System.out.println(solution.findDuplicate(new int[]{2,5,9,6,9,3,8,9,7,1}));
     }
 }
--- src/com/xuhu/LeetCode/mid/midBefore300/mid0287/Solution.java
+++ src/com/xuhu/LeetCode/mid/midBefore300/mid0287/Solution.java
@@ -3,6 +3,19 @@ package com.xuhu.LeetCode.mid.midBefore300.mid0287;
 
 class Solution {
     public int findDuplicate(int[] nums) {
-        return 0;
+        int fast = 0;
+        int slow = 0;
+        while (true){
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+            if (fast == slow){
+                fast = 0;
+                while (fast != slow){
+                    fast = nums[fast];
+                    slow = nums[slow];
+                }
+                return slow;
+            }
+        }
     }
 }
