1055adc9cae24bd971693cafa80d908ea0212e73
xuhu <reallyhwc@foxmail.com>
2020-07-23 22:56:19 +0800

    2020-07-23 0064最小路径和

---
+++ src/com/xuhu/LeetCode/ojEveryDay/Y2020/M07/D23/Main.java
@@ -0,0 +1,10 @@
+package com.xuhu.LeetCode.ojEveryDay.Y2020.M07.D23;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.minPathSum(new int[][]{{1, 3, 1},
+                {1, 5, 1},
+                {4, 2, 1}}));
+    }
+}
---
+++ src/com/xuhu/LeetCode/ojEveryDay/Y2020/M07/D23/Solution.java
@@ -0,0 +1,28 @@
+package com.xuhu.LeetCode.ojEveryDay.Y2020.M07.D23;
+
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+        if (null == grid || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[m - 1][n - 1] = grid[m - 1][n - 1];
+        for (int i = m - 2; i >= 0; i--) {
+            dp[i][n - 1] = grid[i][n - 1] + dp[i + 1][n - 1];
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            dp[m - 1][i] = grid[m - 1][i] + dp[m - 1][i + 1];
+        }
+
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                dp[i][j] = Math.min(dp[i + 1][j], dp[i][j + 1]) + grid[i][j];
+            }
+        }
+
+        return dp[0][0];
+    }
+}
