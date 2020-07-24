a9caaf7a0123dc11d5a6e1615c48d61525278955
xuhu <reallyhwc@foxmail.com>
2020-07-24 16:14:27 +0800

    2020-01-24 1025 除数博弈

---
+++ src/com/xuhu/LeetCode/ojEveryDay/Y2020/M07/D24/Main.java
@@ -0,0 +1,9 @@
+package com.xuhu.LeetCode.ojEveryDay.Y2020.M07.D24;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.divisorGame(2));
+        System.out.println(solution.divisorGame(3));
+    }
+}
---
+++ src/com/xuhu/LeetCode/ojEveryDay/Y2020/M07/D24/Solution.java
@@ -0,0 +1,13 @@
+package com.xuhu.LeetCode.ojEveryDay.Y2020.M07.D24;
+
+
+class Solution {
+    /**
+     * 数学问题 需要推理
+     * @param N
+     * @return
+     */
+    public boolean divisorGame(int N) {
+        return N % 2 == 0;
+    }
+}
