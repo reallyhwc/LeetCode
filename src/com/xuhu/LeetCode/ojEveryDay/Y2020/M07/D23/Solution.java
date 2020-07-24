f0a67a80ec7fa2a33b8352152ee1320a5f482bb7
xuhu <reallyhwc@foxmail.com>
2020-07-24 16:00:51 +0800

    2020-07-23 0064最小路径和 -- 调整注释

--- src/com/xuhu/LeetCode/ojEveryDay/Y2020/M07/D23/Solution.java
+++ src/com/xuhu/LeetCode/ojEveryDay/Y2020/M07/D23/Solution.java
@@ -2,27 +2,28 @@ package com.xuhu.LeetCode.ojEveryDay.Y2020.M07.D23;
 
 
 class Solution {
+    // 又是一道比较经典的DP写法
     public int minPathSum(int[][] grid) {
         if (null == grid || grid.length == 0 || grid[0].length == 0) {
             return 0;
         }
         int m = grid.length;
         int n = grid[0].length;
-        int[][] dp = new int[m][n];
-        dp[m - 1][n - 1] = grid[m - 1][n - 1];
+        // 执行原地算法   首先对于最后一行和最后一列进行边缘计算统计求和
         for (int i = m - 2; i >= 0; i--) {
-            dp[i][n - 1] = grid[i][n - 1] + dp[i + 1][n - 1];
+            grid[i][n - 1] = grid[i][n - 1] + grid[i + 1][n - 1];
         }
         for (int i = n - 2; i >= 0; i--) {
-            dp[m - 1][i] = grid[m - 1][i] + dp[m - 1][i + 1];
+            grid[m - 1][i] = grid[m - 1][i] + grid[m - 1][i + 1];
         }
 
         for (int i = m - 2; i >= 0; i--) {
             for (int j = n - 2; j >= 0; j--) {
-                dp[i][j] = Math.min(dp[i + 1][j], dp[i][j + 1]) + grid[i][j];
+                // 求出从 i j 点开始到结尾的最短距离
+                grid[i][j] = Math.min(grid[i + 1][j], grid[i][j + 1]) + grid[i][j];
             }
         }
 
-        return dp[0][0];
+        return grid[0][0];
     }
 }
