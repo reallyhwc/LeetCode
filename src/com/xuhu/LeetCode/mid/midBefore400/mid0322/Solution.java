8fd1a4f45e6bf5f5c77f6d107d9a10af3230c592
xuhu <reallyhwc@foxmail.com>
2020-07-30 18:58:19 +0800

    0322 零钱兑换 -- 懵逼 为什么这个代码过不了 [2] 3 这个测试用例？ 却能够通过代码

--- src/com/xuhu/LeetCode/mid/midBefore400/mid0322/Solution.java
+++ src/com/xuhu/LeetCode/mid/midBefore400/mid0322/Solution.java
@@ -4,13 +4,47 @@ package com.xuhu.LeetCode.mid.midBefore400.mid0322;
 import java.util.Arrays;
 
 class Solution {
+
+    int ans = Integer.MAX_VALUE;
+
+    public int coinChange(int[] coins, int amount) {
+        // 排序，优先用面额大的
+        Arrays.sort(coins);
+        dfs(coins, coins.length - 1, amount, 0);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+
+    public void dfs(int[] coins, int index, int amount, int cnt) {
+        if (index < 0) {
+            return;
+        }
+        int max = amount / coins[index];
+        // 多往少，因为硬币是从大到小的，所以是尽量用多的大的硬币
+        for (int c = max; c >= 0; c--) {
+            int newAmount = amount - c * coins[index];
+            int newCoinCount = cnt + c;
+
+            // newAmount == 0 时, 得到解答, 记录最小值
+            // 剪枝是因为大面值硬币需要更多小面值硬币替换，继续减少一枚或几枚大硬币搜索出来的答案【如果有】肯定不会更优。
+            if (newAmount == 0) {
+                ans = Math.min(ans, newCoinCount);
+                break;//剪枝1
+            }
+            if (newCoinCount + 1 >= ans) {
+                break; //剪枝2
+            }
+            dfs(coins, index - 1, newAmount, newCoinCount);
+        }
+    }
+
     /**
      * 经典动态规划   还需要任重而道远的学习啊！
+     *
      * @param coins
      * @param amount
      * @return
      */
-    public int coinChange(int[] coins, int amount) {
+    public int coinChange1(int[] coins, int amount) {
         long[] ans = new long[amount + 1];
         Arrays.fill(ans, Integer.MAX_VALUE);
         ans[0] = 0;
