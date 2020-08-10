2e765378b95a3f9761999d23fd013a0644cbb223
xuhu <reallyhwc@foxmail.com>
2020-08-10 19:27:06 +0800

    2020-08-10 0696 计数二进制子串

---
+++ src/com/xuhu/LeetCode/ojEveryDay/Y2020/M08/D10/Main.java
@@ -0,0 +1,9 @@
+package com.xuhu.LeetCode.ojEveryDay.Y2020.M08.D10;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.countBinarySubstrings("00110011"));
+        System.out.println(solution.countBinarySubstrings("00110"));
+    }
+}
---
+++ src/com/xuhu/LeetCode/ojEveryDay/Y2020/M08/D10/Solution.java
@@ -0,0 +1,67 @@
+package com.xuhu.LeetCode.ojEveryDay.Y2020.M08.D10;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+
+    /**
+     * 改进版
+     * @param s
+     * @return
+     */
+    public int countBinarySubstrings(String s) {
+        if (s == null || s.length() == 0){
+            return 0;
+        }
+        char last = s.charAt(0);
+        int pre = 0;
+        int lastNum = 1;
+        int ans = 0;
+        for (int i = 1; i < s.length(); i++){
+            if (last == s.charAt(i)){
+                lastNum++;
+            }else {
+                ans += Math.min(pre,lastNum);
+                last = s.charAt(i);
+                pre = lastNum;
+                lastNum = 1;
+            }
+        }
+        ans += Math.min(pre,lastNum);
+
+        return ans;
+    }
+
+    /**
+     * 先统计连续的0和1分别有多少个，如：111100011000，
+     * 得到4323；在4323中的任意相邻两个数字，取小的一个加起来，就是3+2+2 = 7.
+     * @param s
+     * @return
+     */
+    public int countBinarySubstrings1(String s) {
+        if (s == null || s.length() == 0){
+            return 0;
+        }
+        List<Integer> list = new ArrayList<>();
+        char last = s.charAt(0);
+        int lastNum = 1;
+        for (int i = 1; i < s.length(); i++){
+            if (last == s.charAt(i)){
+                lastNum++;
+            }else {
+                list.add(lastNum);
+                last = s.charAt(i);
+                lastNum = 1;
+            }
+        }
+        list.add(lastNum);
+
+        int ans = 0;
+        for (int i = 1; i < list.size(); i++ ){
+            ans += Math.min(list.get(i-1),list.get(i));
+        }
+        return ans;
+    }
+}
