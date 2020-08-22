9dd8ec76a23f4e7504b9ef0049ced88a056f3e7b
xuhu <reallyhwc@foxmail.com>
2020-08-22 14:59:50 +0800

    2020-08-22 0679 24点游戏

---
+++ src/com/xuhu/LeetCode/ojEveryDay/Y2020/M08/D22/Main.java
@@ -0,0 +1,8 @@
+package com.xuhu.LeetCode.ojEveryDay.Y2020.M08.D22;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.judgePoint24(new int[]{1,2,3,4}));
+    }
+}
