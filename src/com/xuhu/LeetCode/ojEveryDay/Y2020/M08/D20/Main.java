f54a8279453e3aee9c5b19660dac34757af9be93
xuhu <reallyhwc@foxmail.com>
2020-08-23 16:40:33 +0800

    2020-08-20 0529扫雷游戏

---
+++ src/com/xuhu/LeetCode/ojEveryDay/Y2020/M08/D20/Main.java
@@ -0,0 +1,8 @@
+package com.xuhu.LeetCode.ojEveryDay.Y2020.M08.D20;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println();
+    }
+}
