78d7e36b6468a1f1f0ab2fa87e1eba215f3b16b7
xuhu <reallyhwc@foxmail.com>
2020-09-14 13:38:11 +0800

    2020-09-14 0094 二叉树的中序遍历

---
+++ src/com/xuhu/LeetCode/ojEveryDay/Y2020/M09/D14/Main.java
@@ -0,0 +1,11 @@
+package com.xuhu.LeetCode.ojEveryDay.Y2020.M09.D14;
+
+import com.xuhu.LeetCode.a.util.ListUtils;
+import com.xuhu.LeetCode.a.util.TreeUtils;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        ListUtils.printList(solution.inorderTraversal(TreeUtils.create(new Integer[]{1,null,2,null,null,3},0)));
+    }
+}
---
+++ src/com/xuhu/LeetCode/ojEveryDay/Y2020/M09/D14/Solution.java
@@ -0,0 +1,26 @@
+package com.xuhu.LeetCode.ojEveryDay.Y2020.M09.D14;
+
+
+import com.xuhu.LeetCode.a.util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list  = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while(root != null || !stack.isEmpty()){
+            while(root != null){
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            list.add(root.val);
+            root = root.right;
+        }
+
+        return list;
+    }
+}
