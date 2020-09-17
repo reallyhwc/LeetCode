b9ff91be373a8abb8cb0391ebdb0505d3947277f
xuhu <reallyhwc@foxmail.com>
2020-09-17 18:49:25 +0800

    2020-09-17 0685 冗余连接II

---
+++ src/com/xuhu/LeetCode/ojEveryDay/Y2020/M09/D17/Solution.java
@@ -0,0 +1,65 @@
+package com.xuhu.LeetCode.ojEveryDay.Y2020.M09.D17;
+
+class Solution {
+    // 没啥解释了，CV大法吧，现在思路需要转移点到准备面试相关的东西了，hard的，有时间再想想吧
+
+    public int[] findRedundantDirectedConnection(int[][] edges) {
+        int nodesCount = edges.length;
+        UnionFind uf = new UnionFind(nodesCount + 1);
+        int[] parent = new int[nodesCount + 1];
+        for (int i = 1; i <= nodesCount; ++i) {
+            parent[i] = i;
+        }
+        int conflict = -1;
+        int cycle = -1;
+        for (int i = 0; i < nodesCount; ++i) {
+            int[] edge = edges[i];
+            int node1 = edge[0], node2 = edge[1];
+            if (parent[node2] != node2) {
+                conflict = i;
+            } else {
+                parent[node2] = node1;
+                if (uf.find(node1) == uf.find(node2)) {
+                    cycle = i;
+                } else {
+                    uf.union(node1, node2);
+                }
+            }
+        }
+        if (conflict < 0) {
+            int[] redundant = {edges[cycle][0], edges[cycle][1]};
+            return redundant;
+        } else {
+            int[] conflictEdge = edges[conflict];
+            if (cycle >= 0) {
+                int[] redundant = {parent[conflictEdge[1]], conflictEdge[1]};
+                return redundant;
+            } else {
+                int[] redundant = {conflictEdge[0], conflictEdge[1]};
+                return redundant;
+            }
+        }
+    }
+}
+
+class UnionFind {
+    int[] ancestor;
+
+    public UnionFind(int n) {
+        ancestor = new int[n];
+        for (int i = 0; i < n; ++i) {
+            ancestor[i] = i;
+        }
+    }
+
+    public void union(int index1, int index2) {
+        ancestor[find(index1)] = find(index2);
+    }
+
+    public int find(int index) {
+        if (ancestor[index] != index) {
+            ancestor[index] = find(ancestor[index]);
+        }
+        return ancestor[index];
+    }
+}
