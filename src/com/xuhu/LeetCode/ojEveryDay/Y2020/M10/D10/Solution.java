47682576a2720a74f0c8a8b520438ed618753183
xuhu <reallyhwc@foxmail.com>
2020-10-10 15:04:12 +0800

    2020-10-10 0142 环形链表II

---
+++ src/com/xuhu/LeetCode/ojEveryDay/Y2020/M10/D10/Solution.java
@@ -0,0 +1,31 @@
+package com.xuhu.LeetCode.ojEveryDay.Y2020.M10.D10;
+
+
+import com.xuhu.LeetCode.a.util.ListNode;
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null)return null;
+        ListNode p1 = head;
+        ListNode p2 = head;
+        boolean hasCycle = false;
+        while (p2.next != null && p2.next.next != null){
+            p1 = p1.next;
+            p2 = p2.next.next;
+            if (p1 == p2){
+                hasCycle = true;
+                break;
+            }
+        }
+        if(hasCycle){
+            ListNode q = head;
+            while (q != p1){
+                q = q.next;
+                p1 = p1.next;
+            }
+            return q;
+        }else {
+            return null;
+        }
+    }
+}
