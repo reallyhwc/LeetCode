8b6644ac179dcf616a489b795967f36495d2d56c
xuhu <reallyhwc@foxmail.com>
2020-10-23 23:10:50 +0800

    2020-10-23 0234 回文链表  - 手动再写一遍更高阶的做法

--- src/com/xuhu/LeetCode/ojEveryDay/Y2020/M10/D23/Main.java
+++ src/com/xuhu/LeetCode/ojEveryDay/Y2020/M10/D23/Main.java
@@ -1,11 +1,17 @@
 package com.xuhu.LeetCode.ojEveryDay.Y2020.M10.D23;
 
+import com.xuhu.LeetCode.a.util.ListNode;
 import com.xuhu.LeetCode.a.util.ListNodeUtil;
+import com.xuhu.LeetCode.a.util.ListUtils;
 
 public class Main {
     public static void main(String[] args){
         Solution solution = new Solution();
-        System.out.println(solution.isPalindrome(ListNodeUtil.convertArrayToListNode(new int[]{1,1,2,2,3,3,4,3,3,2,2,1,1})));
-        System.out.println(solution.isPalindrome(ListNodeUtil.convertArrayToListNode(new int[]{1,1,2,2,3,3,4,3,3,2,2,1})));
+//        System.out.println(solution.isPalindrome();
+//        System.out.println(solution.isPalindrome(ListNodeUtil.convertArrayToListNode(new int[]{1,1,2,2,3,3,4,3,3,2,2,1})));
+        ListNode node = ListNodeUtil.convertArrayToListNode(new int[]{1,1,2,2,3,3,4,3,3,2,2,1,1});
+
+        System.out.println(solution.isPalindrome(node));
+
     }
 }
--- src/com/xuhu/LeetCode/ojEveryDay/Y2020/M10/D23/Solution.java
+++ src/com/xuhu/LeetCode/ojEveryDay/Y2020/M10/D23/Solution.java
@@ -4,7 +4,67 @@ package com.xuhu.LeetCode.ojEveryDay.Y2020.M10.D23;
 import com.xuhu.LeetCode.a.util.ListNode;
 
 class Solution {
+    /**
+     * 判断是否为回文链表 拷贝数据做法：
+     * 原地做法：
+     * 首先找到中点截取前后端链表 反转后段链表然后再讲两段链表进行比较
+     * 比较完成之后再把后半段反转回来拼接回去（避免对入参产生影响）
+     * @param head 待判断链表
+     * @return 当前链表是否为回文链表
+     */
     public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null){
+            return true;
+        }
+        ListNode mid = this.getMid(head);
+        ListNode end = this.reverseListNode(mid.next);
+        ListNode begin = head;
+
+        ListNode tempEnd = end;
+
+        while (end != null && end.val == begin.val){
+            end = end.next;
+            begin = begin.next;
+        }
+
+        mid.next = reverseListNode(tempEnd);
+        return end == null;
+    }
+
+    public ListNode getMid(ListNode node){
+        if (node == null || node.next == null) {
+            return node;
+        }
+        ListNode p1 = node;
+        ListNode p2 = node.next;
+        while (p2 != null && p2.next != null){
+            p1 = p1.next;
+            p2 = p2.next.next;
+        }
+        return p1;
+    }
+
+    public ListNode reverseListNode(ListNode node){
+        if (node == null || node.next == null) {
+            return node;
+        }
+        ListNode ans = null;
+        while (node != null){
+            ListNode temp = node.next;
+            node.next = ans;
+            ans = node;
+            node = temp;
+        }
+        return ans;
+    }
+
+    /**
+     * 判断是否为回文链表 拷贝数据做法：
+     * 拷贝翻转一个链表reverse 然后再比对数据
+     * @param head 待判断链表
+     * @return 当前链表是否为回文链表
+     */
+    public boolean isPalindrome1(ListNode head) {
         if (head == null || head.next == null){
             return true;
         }
