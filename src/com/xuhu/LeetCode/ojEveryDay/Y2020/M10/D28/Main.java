b391945b102293c65a3b2d9c00e771a18e9cac6c
xuhu <reallyhwc@foxmail.com>
2020-10-28 20:37:17 +0800

    2020-10-28 1207 独一无二的出现次数

---
+++ src/com/xuhu/LeetCode/ojEveryDay/Y2020/M10/D28/Main.java
@@ -0,0 +1,10 @@
+package com.xuhu.LeetCode.ojEveryDay.Y2020.M10.D28;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.uniqueOccurrences(new int[]{1,2,2,1,1,3}));
+        System.out.println(solution.uniqueOccurrences(new int[]{1,2}));
+        System.out.println(solution.uniqueOccurrences(new int[]{-3,0,1,-3,1,1,1,-3,10,0}));
+    }
+}
---
+++ src/com/xuhu/LeetCode/ojEveryDay/Y2020/M10/D28/Solution.java
@@ -0,0 +1,26 @@
+package com.xuhu.LeetCode.ojEveryDay.Y2020.M10.D28;
+
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int num : arr){
+            map.put(num,map.getOrDefault(num,0) + 1);
+        }
+        Set<Integer> set = new HashSet<>();
+
+        for (Map.Entry< Integer, Integer > entry : map.entrySet()){
+            if (set.contains(entry.getValue())){
+                return false;
+            }else {
+                set.add(entry.getValue());
+            }
+        }
+        return true;
+    }
+}
