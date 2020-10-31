1ff1df4c0059c03650c3ade32139079938a93bae
xuhu <reallyhwc@foxmail.com>
2020-10-31 22:28:48 +0800

    0402 移掉k位数字

---
+++ src/com/xuhu/LeetCode/mid/midBefore500/mid0402/Main.java
@@ -0,0 +1,10 @@
+package com.xuhu.LeetCode.mid.midBefore500.mid0402;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.removeKdigits("1432219",3));
+        System.out.println(solution.removeKdigits("10200",1));
+        System.out.println(solution.removeKdigits("12",2));
+    }
+}
---
+++ src/com/xuhu/LeetCode/mid/midBefore500/mid0402/Solution.java
@@ -0,0 +1,26 @@
+package com.xuhu.LeetCode.mid.midBefore500.mid0402;
+
+
+class Solution {
+    public String removeKdigits(String num, int k) {
+        if (num == null || num.length() == 0){
+            return num;
+        }
+        if (num.length() == k){
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder(num);
+        // 思路，从左到右，找出第一个比后面大的字符 删去，清零 重复k次
+        for (int i = 0; i < k; i++){
+            int idx = 0;
+            for (int j = 1; j < sb.length() && sb.charAt(j) >= sb.charAt(j-1); j++) {
+                idx = j;
+            }
+            sb.deleteCharAt(idx);
+            while (sb.length() > 1 && sb.charAt(0) == '0'){
+                sb.deleteCharAt(0);
+            }
+        }
+        return sb.toString();
+    }
+}
