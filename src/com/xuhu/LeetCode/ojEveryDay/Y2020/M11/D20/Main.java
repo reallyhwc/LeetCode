ec6007de81e978a97742d93280bf2e59121b59e6
xuhu <reallyhwc@foxmail.com>
2020-11-23 12:45:26 +0800

    2020-11-20 0147 对链表进行插入排序 -- 调整注释并手写一遍

--- src/com/xuhu/LeetCode/ojEveryDay/Y2020/M11/D20/Main.java
+++ src/com/xuhu/LeetCode/ojEveryDay/Y2020/M11/D20/Main.java
@@ -1,8 +1,10 @@
 package com.xuhu.LeetCode.ojEveryDay.Y2020.M11.D20;
 
+import com.xuhu.LeetCode.a.util.ListNodeUtil;
+
 public class Main {
     public static void main(String[] args){
         Solution solution = new Solution();
-        System.out.println();
+        ListNodeUtil.printListNode(solution.insertionSortList(ListNodeUtil.convertArrayToListNode(new int[]{4,2,1,3})));
     }
 }
--- src/com/xuhu/LeetCode/ojEveryDay/Y2020/M11/D20/Solution.java
+++ src/com/xuhu/LeetCode/ojEveryDay/Y2020/M11/D20/Solution.java
@@ -3,25 +3,29 @@ package com.xuhu.LeetCode.ojEveryDay.Y2020.M11.D20;
 
 import com.xuhu.LeetCode.a.util.ListNode;
 
+import java.util.List;
+
 class Solution {
     public ListNode insertionSortList(ListNode head) {
         ListNode dummy = new ListNode(Integer.MIN_VALUE);
         ListNode pre;
         dummy.next = head;
-        while (head != null && head.next != null) {
-            if (head.val <= head.next.val) {
+        while (head != null && head.next != null){
+            if (head.val < head.next.val){
                 head = head.next;
                 continue;
             }
             pre = dummy;
-            while (pre.next.val < head.next.val) {
+            while (pre.next.val < head.next.val){
                 pre = pre.next;
             }
             ListNode curr = head.next;
             head.next = curr.next;
             curr.next = pre.next;
             pre.next = curr;
+
         }
+
         return dummy.next;
     }
 }
