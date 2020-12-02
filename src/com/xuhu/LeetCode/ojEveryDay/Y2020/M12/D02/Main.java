1dd140c1eeca805655f80406103626fad35c1286
xuhu <reallyhwc@foxmail.com>
2020-12-02 19:00:56 +0800

    2020-12-02 0321 拼接最大数

---
+++ src/com/xuhu/LeetCode/ojEveryDay/Y2020/M12/D02/Main.java
@@ -0,0 +1,10 @@
+package com.xuhu.LeetCode.ojEveryDay.Y2020.M12.D02;
+
+import com.xuhu.LeetCode.a.util.ListUtils;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        ListUtils.pringlnArrays(solution.maxNumber(new int[]{3, 4, 6, 5}, new int[]{9, 1, 2, 5, 8, 3},5));
+    }
+}
