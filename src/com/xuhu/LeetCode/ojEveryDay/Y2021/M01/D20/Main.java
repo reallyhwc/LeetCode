996172b72d44dadd7914996c48a581286d941103
xuhu <reallyhwc@foxmail.com>
2021-01-20 21:33:05 +0800

    2021-01-20 0628 三个数的最大乘积

---
+++ src/com/xuhu/LeetCode/ojEveryDay/Y2021/M01/D20/Main.java
@@ -0,0 +1,9 @@
+package com.xuhu.LeetCode.ojEveryDay.Y2021.M01.D20;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.maximumProduct(new int[]{1,2,3}));
+        System.out.println(solution.maximumProduct(new int[]{1,2,3,4}));
+    }
+}
