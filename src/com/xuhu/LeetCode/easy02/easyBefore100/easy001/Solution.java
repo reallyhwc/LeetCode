0b50ad7aab3ce341f8afbff4bddaab7c0886ae71
xuhu <xuhu.xu@alibaba-inc.com>
2025-06-30 16:53:38 +0800

    feat(easy001): 新增两数之和功能实现及主程序入口

---
+++ src/com/xuhu/LeetCode/easy02/easyBefore100/easy001/Solution.java
@@ -0,0 +1,17 @@
+package com.xuhu.LeetCode.easy02.easyBefore100.easy001;
+
+
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new java.util.HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[] {map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
--- src/com/xuhu/LeetCode/mid/midBefore100/mid0002/Main.java
+++ src/com/xuhu/LeetCode/mid/midBefore100/mid0002/Main.java
@@ -3,9 +3,22 @@ package com.xuhu.LeetCode.mid.midBefore100.mid0002;
 import com.xuhu.LeetCode.a.util.ListNode;
 import com.xuhu.LeetCode.a.util.ListNodeUtil;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args){
-        Solution solution = new Solution();
-        ListNodeUtil.printListNode(solution.addTwoNumbers(ListNodeUtil.convertArrayToListNode(new int[]{2,4,3}),ListNodeUtil.convertArrayToListNode(new int[]{5,6,9})));
+//        Solution solution = new Solution();
+//        ListNodeUtil.printListNode(solution.addTwoNumbers(ListNodeUtil.convertArrayToListNode(new int[]{2,4,3}),ListNodeUtil.convertArrayToListNode(new int[]{5,6,9})));
+
+        Boolean test = null;
+        Long test2 = 1L;
+
+        Set<Long> set = new HashSet<>();
+        set.add(1L);
+
+        Boolean b = test && set.contains(test2);
+
+        System.out.println();
     }
 }
