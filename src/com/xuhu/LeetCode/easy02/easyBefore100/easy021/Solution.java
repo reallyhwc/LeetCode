f18eb138033c5c70c523445f669884e6f0e82ca7
xuhu <reallyhwc@foxmail.com>
2025-07-14 23:14:10 +0800

    easy21

---
+++ src/com/xuhu/LeetCode/easy02/easyBefore100/easy021/Solution.java
@@ -0,0 +1,35 @@
+package com.xuhu.LeetCode.easy02.easyBefore100.easy021;
+
+
+import com.xuhu.LeetCode.a.util.ListNode;
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        ListNode temp = new ListNode(-1);
+        ListNode cur = temp;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                cur.next = list1;
+                list1 = list1.next;
+                cur = cur.next;
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
+                cur = cur.next;
+            }
+        }
+        if (list1 != null) {
+            cur.next = list1;
+        }
+        if (list2 != null) {
+            cur.next = list2;
+        }
+        return temp.next;
+    }
+}
