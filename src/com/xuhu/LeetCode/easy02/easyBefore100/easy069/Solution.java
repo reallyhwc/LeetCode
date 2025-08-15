1f36bcbfa21f3228c91c1150318335cb7ea150f4
xuhu <xuhu.xu@alibaba-inc.com>
2025-08-15 09:15:04 +0800

    easy069

--- src/com/xuhu/LeetCode/easy02/easyBefore100/easy069/Solution.java
+++ src/com/xuhu/LeetCode/easy02/easyBefore100/easy069/Solution.java
@@ -4,7 +4,7 @@ package com.xuhu.LeetCode.easy02.easyBefore100.easy069;
 class Solution {
     public int mySqrt(int a) {
         long x0 = a;
-
+        // 牛顿迭代法快速求解，正常也可以使用二分法持续迭代
         while (x0 * x0 > a) {
             x0 = (x0 + a / x0) / 2;
         }
