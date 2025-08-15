db6f67ce07cfc2e501d86ea4d256ec3400555021
xuhu <xuhu.xu@alibaba-inc.com>
2025-08-15 09:20:56 +0800

    easy070

---
+++ src/com/xuhu/LeetCode/easy02/easyBefore100/easy070/Solution.java
@@ -0,0 +1,18 @@
+package com.xuhu.LeetCode.easy02.easyBefore100.easy070;
+
+
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int first = 1;
+        int second = 2;
+        for (int i = 3; i <= n; i++) {
+            int temp = first + second;
+            first = second;
+            second = temp;
+        }
+        return second;
+    }
+}
