745583e094a6117906f4e8f528b2c8eff68c4c98
xuhu <xuhu.xu@alibaba-inc.com>
2025-08-19 09:22:28 +0800

    easy100

---
+++ src/com/xuhu/LeetCode/easy02/easyBefore100/easy100/Solution.java
@@ -0,0 +1,16 @@
+package com.xuhu.LeetCode.easy02.easyBefore100.easy100;
+
+
+import com.xuhu.LeetCode.a.util.TreeNode;
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
