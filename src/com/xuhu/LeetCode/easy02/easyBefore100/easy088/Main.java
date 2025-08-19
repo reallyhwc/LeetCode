da1aa9ac398f1450256e4bee37cf2076272c77c2
xuhu <xuhu.xu@alibaba-inc.com>
2025-08-19 09:12:17 +0800

    easy088

---
+++ src/com/xuhu/LeetCode/easy02/easyBefore100/easy088/Main.java
@@ -0,0 +1,9 @@
+package com.xuhu.LeetCode.easy02.easyBefore100.easy088;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.merge(new int[]{0}, 0, new int[]{1}, 1);
+        System.out.println();
+    }
+}
