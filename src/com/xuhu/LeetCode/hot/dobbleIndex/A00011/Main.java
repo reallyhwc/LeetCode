b3207c47ae5b0a41b009582a220ea1d7cb1fe69c
xuhu <xuhu.xu@alibaba-inc.com>
2025-08-27 20:16:29 +0800

    11

---
+++ src/com/xuhu/LeetCode/hot/dobbleIndex/A00011/Main.java
@@ -0,0 +1,8 @@
+package com.xuhu.LeetCode.hot.dobbleIndex.A00011;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println();
+    }
+}
---
+++ src/com/xuhu/LeetCode/hot/dobbleIndex/A00011/Solution.java
@@ -0,0 +1,19 @@
+package com.xuhu.LeetCode.hot.dobbleIndex.A00011;
+
+
+public class Solution {
+    public int maxArea(int[] height) {
+        int l = 0, r = height.length - 1;
+        int ans = 0;
+        while (l < r) {
+            int area = Math.min(height[l], height[r]) * (r - l);
+            ans = Math.max(ans, area);
+            if (height[l] <= height[r]) {
+                ++l;
+            } else {
+                --r;
+            }
+        }
+        return ans;
+    }
+}
